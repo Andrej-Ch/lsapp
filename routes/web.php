41fc9dbae173dad7dedcb39c03364b4656da3de0
Andrej_Ch <andrej@vergelijkdirect.com>
2017-09-15 16:31:26 +0200

    lsapp tutorial

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//Route:: get('/about/{id}/{name}', function($id, $name){
+//    return 'This is user '.$id . '. His name is ' . $name;
+//});
+
+/*Route::get('/', function () {
+    //return view('welcome');
+   // return "Hello world";
+    return '<h1>header</h1>';
+});*/
+
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+
